bd03d80883f5c6b3879aca719bf6bc195a608a46
Shweta1312 <shawant11@gmail.com>
2020-10-23 15:29:05 +0530

    Sorting number array with bubble sort

--- build/index.js
+++ build/index.js
@@ -1,6 +1,24 @@
 "use strict";
-console.log('Hey there');
-var func = function () {
-    console.log("Hello");
-};
-func();
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        //Object destructuring
+        var length = this.collection.length;
+        //Bubble sort
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, -2, 4, 1, 7]);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
-console.log('Hey there');
+class Sorter {
+  constructor(public collection: number[]) {}
 
-const func = () => {
-  console.log('Hello');
-};
+  sort() {
+    //Object destructuring
+    const { length } = this.collection;    
 
-func();
+    //Bubble sort
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+
+    
+
+  }
+}
+
+
+
+
+
+const sorter = new Sorter([10, -2, 4, 1, 7]);
+
+sorter.sort();
+
+console.log(sorter.collection);
